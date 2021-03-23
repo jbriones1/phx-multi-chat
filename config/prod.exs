f28f38678660bb0ebad6723475de8801580cf07b
Jon Andre Briones <jbriones1@my.bcit.ca>
2021-03-22 22:06:40 -0700

    Changed database name

--- config/prod.exs
+++ config/prod.exs
@@ -18,7 +18,7 @@ config :chat, ChatWeb.Endpoint,
   secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE")
 
 # Configure your database
-config :chat, Chat.Repo,
+config :multi_chat, Chat.Repo,
   adapter: Ecto.Adapters.Postgres,
   url: System.get_env("DATABASE_URL"),
   pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
