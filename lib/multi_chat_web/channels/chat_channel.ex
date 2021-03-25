71c50b42a1262a0607b024f5a71d04b5879d9f72
Jon Andre Briones <jbriones1@my.bcit.ca>
2021-03-25 12:34:31 -0700

    Added a joined message

--- lib/multi_chat_web/channels/chat_channel.ex
+++ lib/multi_chat_web/channels/chat_channel.ex
@@ -5,15 +5,16 @@ defmodule MultiChatWeb.ChatChannel do
 
   @impl true
   def join("chat:" <> _room, _payload, socket) do
+      push(socket, "joined", %{"message" => "hello"})
       {:ok, socket}
   end
 
   @impl true
   def handle_in("shout", payload, socket) do
-    "chat:" <> room = socket.topic
-    payload = Map.merge(payload, %{"room" => room})
-    Chats.create_message(payload)
-    broadcast socket, "shout", payload
+    "chat:" <> room = socket.topic # retrieve the subtopic
+    payload = Map.merge(payload, %{"room" => room}) # append the room name to the payload
+    Chats.create_message(payload) # store the payload into the database
+    broadcast socket, "shout", payload # broadcast the payload
     {:noreply, socket}
   end
 end
